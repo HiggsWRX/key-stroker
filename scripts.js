f3a894f756bef50740d4c213c5346071be93390b
Hiago <hiago93@outlook.com>
2015-12-29 18:59:12 +0000

    Added remaining assets

--- scripts.js
+++ scripts.js
@@ -6,10 +6,22 @@ var numKs = 0;
 var cash = 0;
 var numElder = 0;
 var numKid = 0;
+var numStud = 0;
+var numCont = 0;
+var numChamp = 0;
+var numRob = 0;
 var elderUnlocked = false;
 var kidUnlocked = false;
+var studUnlocked = false;
+var contUnlocked = false;
+var champUnlocked = false;
+var robUnlocked = false;
 var elderCost = 15;
 var kidCost = 100;
+var studCost = 500;
+var contCost = 3000;
+var champCost = 10000;
+var robCost = 40000;
 
 // increase the number of keystrokes when user presses a key
 // between a and z
@@ -52,6 +64,66 @@ window.setInterval(function () {
 
         kidUnlocked = true;
     }
+    
+    if (cash >= 500 && !studUnlocked) {
+        var stud = document.createElement('div');
+        stud.innerHTML = '<button id="hire-stud">Hire Student</button><span id="stud-price">' + studCost + '</span>';
+        document.getElementById('assets').appendChild(stud);
+
+        document.getElementById('hire-stud').addEventListener('click', function (event) {
+            numStud++;
+            cash -= studCost;
+            studCost = Math.ceil(studCost * 1.3);
+            kps += 40;
+        });
+
+        studUnlocked = true;
+    }
+    
+    if (cash >= 3000 && !contUnlocked) {
+        var cont = document.createElement('div');
+        cont.innerHTML = '<button id="hire-cont">Hire Contractor</button><span id="cont-price">' + contCost + '</span>';
+        document.getElementById('assets').appendChild(cont);
+
+        document.getElementById('hire-cont').addEventListener('click', function (event) {
+            numCont++;
+            cash -= contCost;
+            contCost = Math.ceil(contCost * 1.2);
+            kps += 100;
+        });
+
+        contUnlocked = true;
+    }
+    
+    if (cash >= 10000 && !champUnlocked) {
+        var champ = document.createElement('div');
+        champ.innerHTML = '<button id="hire-champ">Hire champ</button><span id="champ-price">' + champCost + '</span>';
+        document.getElementById('assets').appendChild(champ);
+
+        document.getElementById('hire-champ').addEventListener('click', function (event) {
+            numChamp++;
+            cash -= champCost;
+            champCost = Math.ceil(champCost * 1.2);
+            kps += 400;
+        });
+
+        champUnlocked = true;
+    }
+    
+    if (cash >= 40000 && !robUnlocked) {
+        var rob = document.createElement('div');
+        rob.innerHTML = '<button id="hire-rob">Hire rob</button><span id="rob-price">' + robCost + '</span>';
+        document.getElementById('assets').appendChild(rob);
+
+        document.getElementById('hire-rob').addEventListener('click', function (event) {
+            numRob++;
+            cash -= robCost;
+            robCost = Math.ceil(robCost * 1.2);
+            kps += 1000;
+        });
+
+        robUnlocked = true;
+    }
 
     // disable button if cash is not enough to buy the elder
     if (elderUnlocked) {
@@ -69,6 +141,42 @@ window.setInterval(function () {
             document.getElementById("hire-kid").disabled = true;
         } else {
             document.getElementById("hire-kid").disabled = false;
+        }
+    }
+    
+    if (studUnlocked) {
+        document.getElementById("stud-price").textContent = studCost;
+        if (cash < studCost) {
+            document.getElementById("hire-stud").disabled = true;
+        } else {
+            document.getElementById("hire-stud").disabled = false;
+        }
+    }
+    
+    if (contUnlocked) {
+        document.getElementById("cont-price").textContent = contCost;
+        if (cash < contCost) {
+            document.getElementById("hire-cont").disabled = true;
+        } else {
+            document.getElementById("hire-cont").disabled = false;
+        }
+    }
+    
+    if (champUnlocked) {
+        document.getElementById("champ-price").textContent = champCost;
+        if (cash < champCost) {
+            document.getElementById("hire-champ").disabled = true;
+        } else {
+            document.getElementById("hire-champ").disabled = false;
+        }
+    }
+    
+    if (robUnlocked) {
+        document.getElementById("rob-price").textContent = robCost;
+        if (cash < robCost) {
+            document.getElementById("hire-rob").disabled = true;
+        } else {
+            document.getElementById("hire-rob").disabled = false;
         }
     }
 
